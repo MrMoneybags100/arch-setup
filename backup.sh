3819283df19903b42a654813bafb38184103abbb
MrMoneybags100 <cosmicduck10@gmail.com>
2026-06-16 00:48:31 +0100

    few more dotfile restore changes.

--- backup.sh
+++ backup.sh
@@ -7,36 +7,35 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Backing up dotfiles..."
 
 
-# KDE
-mkdir -p "$ROOT_DIR/backup/dotfiles/kde"
+mkdir -p "$ROOT_DIR/backup/dotfiles/config"
 
+# KDE
 # General KDE appearance
 cp ~/.config/kdeglobals \
-	"$ROOT_DIR/backup/dotfiles/kde/"
+	"$ROOT_DIR/backup/dotfiles/config/"
 
 # Window management
 cp ~/.config/kwinrc \
-	"$ROOT_DIR/backup/dotfiles/kde/"
+	"$ROOT_DIR/backup/dotfiles/config/"
 
 # Desktop layout
 cp ~/.config/plasma-org.kde.plasma.desktop-appletsrc \
-	"$ROOT_DIR/backup/dotfiles/kde/"
+	"$ROOT_DIR/backup/dotfiles/config/"
 
 # Global shortcuts
 cp ~/.config/kglobalshortcutsrc \
-	"$ROOT_DIR/backup/dotfiles/kde/"
+	"$ROOT_DIR/backup/dotfiles/config/"
 
 # Lock screen wallpaper set
 cp ~/.config/kglobalshortcutsrc \
-        "$ROOT_DIR/backup/dotfiles/kde/"
+        "$ROOT_DIR/backup/dotfiles/config/"
 
 
 # BASH
-mkdir -p "$ROOT_DIR/backup/dotfiles/bash"
 
 # Bash alias's/customisation
 cp ~/.bashrc \
-	"$ROOT_DIR/backup/dotfiles/bash/"
+	"$ROOT_DIR/backup/dotfiles/"
 
 
 
